e8e87ac232a464b3ec97f054e777482b2d5fa525
Azzi-Mo <alhwarymostafa@gmail.com>
2023-09-06 10:54:48 +0300

    edit

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,13 +1,15 @@
 "use client";
 import React from "react";
 import { CustomButtonProps } from "@/types";
-
+import Image from "next/image";
 const Button = ({
   title,
   containerStyles,
   handleClick,
   isDisabled,
-  btnType
+  btnType,
+  textStyles,
+  rightIcon
 }: CustomButtonProps) => (
 
     <button
@@ -16,7 +18,17 @@ const Button = ({
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className="flex-1">{title}</span>
+      <span className={`flex-1 ${textStyles}`} >{title}</span>
+      {rightIcon && (
+      <div className="relative w-6 h-6">
+        <Image
+          src={rightIcon}
+          alt="arrow_left"
+          fill
+          className="object-contain"
+        />
+      </div>
+    )}
     </button>
 
 )
--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,5 @@
 import { MouseEventHandler } from "react";
+import { StringLiteral } from "typescript";
 
 export interface CustomButtonProps {
   isDisabled?: boolean;
@@ -14,3 +15,19 @@ export interface SearchManuFacturerProps {
   manufacturer: string;
   setManuFacturer: (manufacturer: string) => void;
 }
+
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
+
